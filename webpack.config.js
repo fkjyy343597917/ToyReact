7df4e415b181dd30db7d2aa342c4bffa1ad4d44b
fangkj <343597917@qq.com>
2020-09-09 09:55:12 +0800

    toy react

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    entry: {
+        main: './main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: [['@babel/plugin-transform-react-jsx', {pragma: 'createElement'}]]
+                    }
+                }
+            }
+        ]
+    },
+    //编译出可看懂的js
+    mode: "development",
+    optimization: {
+        minimize: false
+    }
+}
